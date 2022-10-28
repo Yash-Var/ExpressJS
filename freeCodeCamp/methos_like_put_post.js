c1851f6d3d7f6e5d8358443493daaee392162fe7
Yash Varshney <yash.2125cs1155@kiet.edu>
2022-10-28 19:57:03 +0530

    javascript form completed

--- freeCodeCamp/methos_like_put_post.js
+++ freeCodeCamp/methos_like_put_post.js
@@ -2,16 +2,28 @@ const express = require("express");
 
 const app = express();
 
-let { products } = require("./data");
+let { people } = require("./data");
 
 app.use(express.static("./method_public"));
 
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/api", (req, res) => {
-  res.status(200).json({ sucess: true, yash: products });
+app.use(express.json())
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
+app.post('/api/people', (req, res) => {
+    const { name } = req.body
+    console.log(req.body);
+    if (!name) {
+      return res
+        .status(400)
+        .json({ success: false, msg: 'please provide name value' })
+    }
+    res.status(201).json({ success: true, person: name })
+  })
 app.post("/login", (req, res) => {
   const {name}=req.body;
   if(name){
