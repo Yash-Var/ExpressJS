c034f0b126d8071c60100ca60e03c0d1b17e28ff
Yash Varshney <yash.2125cs1155@kiet.edu>
2022-10-28 12:02:51 +0530

    params

---
+++ freeCodeCamp/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const readfile = require("fs");
+const app = express();
+const { products } = require("./data");
+const Port = 8000;
+app.get("/", (req, res) => {
+  res.send('<h1>Home Page</h1><a href="/api/product">Product</a>');
+});
+app.get("/api/product", (req, res) => {
+  const newProduct = products.map((products) => {
+    const { id, name, image } = products;
+    return { id, name, image };
+  });
+  res.json(newProduct);
+});
+app.get("/api/product/:productID", (req, res) => {
+  const { productID } = req.params;
+  const singleProduct = products.find((products) => {
+    // console.log(Number(productID));
+
+    return products.id === Number(productID);
+  });
+  if (!singleProduct) {
+    return res.status(404).send("Not Found");
+  }
+  res.json(singleProduct);
+});
+// app.all('*',(req,res)=>{
+//     res.send("Page Not Found");
+// })
+app.listen(Port, () => {
+  console.log(`Server is Running on ${Port}`);
+});
