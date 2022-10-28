f1810266cf8c5664f28c1b83a271e2e825ac2909
Yash Varshney <yash.2125cs1155@kiet.edu>
2022-10-28 19:05:50 +0530

    post method completed by from method

--- freeCodeCamp/methos_like_put_post.js
+++ freeCodeCamp/methos_like_put_post.js
@@ -1,12 +1,25 @@
-const express=require('express')
+const express = require("express");
 
-const app=express();
+const app = express();
 
-let {people}=require('./data')
- 
-app.get('/api',(req,res)=>{
-res.status(200).json({sucess:true,data:people})
-})
- app.listen(6066,()=>{
-    console.log(`server is running on port 6060`);
- })
+let { products } = require("./data");
+
+app.use(express.static("./method_public"));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api", (req, res) => {
+  res.status(200).json({ sucess: true, yash: products });
+});
+
+app.post("/login", (req, res) => {
+  const {name}=req.body;
+  if(name){
+    return res.status(200).send(`welcome ${name}`)
+  }
+  else 
+  return res.status(401).send("Please Provide Credentials....")
+});
+app.listen(6066, () => {
+  console.log(`server is running on port 6060`);
+});
